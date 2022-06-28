69f20933c2c13be4c3ccc2247f02f1535653f564
Brian4755 <brian.mccune1@yahoo.com>
2022-06-28 14:34:19 -0500

    navbar complete

--- routes/addreviews.js
+++ routes/addreviews.js
@@ -3,6 +3,9 @@ import * as addreviewsCtrl from '../controllers/addreviews.js'
 
 const router = Router()
 
+// GET localhost:3000/addreviews
+router.get('/', addreviewsCtrl.index)
+
 // POST localhost:3000/addreviews
 router.post('/', addreviewsCtrl.create)
 
